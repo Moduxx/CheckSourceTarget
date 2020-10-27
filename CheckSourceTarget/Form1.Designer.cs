b7f170ee6120ae97f8c053e8f6c594aa296d0032
Modestas <modestas.dulevicius@simplanova.com>
2020-10-27 11:02:07 +0200

    Added new project
    Added new project, implemented value splitter for source and target nodes

---
+++ CheckSourceTarget/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CheckSourceTarget
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(282, 309);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(210, 40);
+            this.CheckButton.TabIndex = 0;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(192, 209);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(427, 20);
+            this.pathTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.nameLabel.Location = new System.Drawing.Point(252, 78);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(277, 26);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Match Sources and Targets";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(616, 207);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(32, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "..";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.CheckButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ CheckSourceTarget/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CheckSourceTarget
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            if (pathTextBox.Text != "")
+            {
+                var filePath = string.Empty;
+                var fileContent = string.Empty;
+
+                using (StreamReader reader = new StreamReader(pathTextBox.Text))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+
+                string[] lines = Regex.Split(fileContent, Environment.NewLine);
+
+                string[] targetValues = new string[(lines.Count() + 1)];
+                string[] sourceValues = new string[(lines.Count() + 1)];
+
+                int k = 0;
+
+                foreach (var line in lines)
+                {
+                    if (line.StartsWith("          <source>"))
+                    {
+                        string[] firstSplit = line.Split('>');
+                        string[] secondSplit = firstSplit[1].Split('<');
+                        sourceValues[k] = secondSplit[0];
+                    }
+                    else if (line.StartsWith("          <target>"))
+                    {
+                        string[] firstSplit = line.Split('>');
+                        string[] secondSplit = firstSplit[1].Split('<');
+                        targetValues[k] = secondSplit[0];
+                        k++;
+                    }
+                }
+                MessageBox.Show(string.Format("Amount of lines procceed: {0}", k));
+            }
+            else
+                MessageBox.Show("Please select translation file path!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "XLIFF (*.xlf)|*.xlf|All files (*.*)|*.*";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pathTextBox.Text = ofd.FileName;
+                }
+            }
+        }
+    }
+}
